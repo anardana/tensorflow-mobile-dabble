e20bb3ee5432f47d891b8804260aea4041ef5f6d
Ankit Srivastava <33308103+anardana@users.noreply.github.com>
2018-03-13 12:12:19 +0530

    Beautification done

--- tensorflow/examples/android/src/org/tensorflow/demo/RecognitionScoreView.java
+++ tensorflow/examples/android/src/org/tensorflow/demo/RecognitionScoreView.java
@@ -16,6 +16,7 @@ limitations under the License.
 package org.tensorflow.demo;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -23,6 +24,7 @@ import android.graphics.Typeface;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.View;
+import android.widget.ImageView;
 
 import org.tensorflow.demo.Classifier.Recognition;
 import org.tensorflow.demo.entities.ObjectWithDetection;
@@ -36,10 +38,12 @@ public class RecognitionScoreView extends View implements ResultsView {
     private static final float TEXT_SIZE_DIP = 12;
     private final float textSizePx;
     private final Paint fgPaint;
+    private final Paint fgPaintHeader;
     private final Paint headerPaint;
     private final Paint bgPaint;
     private List<Recognition> results;
     private ObjectWithDetection image;
+    private Bitmap bitmap;
 
 
     public RecognitionScoreView(final Context context, final AttributeSet set) {
@@ -49,12 +53,19 @@ public class RecognitionScoreView extends View implements ResultsView {
                 TypedValue.applyDimension(
                         TypedValue.COMPLEX_UNIT_DIP, TEXT_SIZE_DIP, getResources().getDisplayMetrics());
         fgPaint = new Paint();
-        fgPaint.setTypeface(Typeface.create(Typeface.MONOSPACE, Typeface.BOLD));
+        fgPaint.setTypeface(Typeface.create(Typeface.SANS_SERIF, Typeface.NORMAL));
         fgPaint.setTextSize(textSizePx);
+        fgPaint.setColor(Color.WHITE);
+
+        fgPaintHeader = new Paint();
+        fgPaintHeader.setTypeface(Typeface.create(Typeface.SANS_SERIF, Typeface.BOLD));
+        fgPaintHeader.setTextSize(textSizePx);
+        fgPaintHeader.setColor(Color.WHITE);
 
         headerPaint = new Paint();
-        headerPaint.setTypeface(Typeface.create(Typeface.MONOSPACE, Typeface.BOLD));
+        headerPaint.setTypeface(Typeface.create(Typeface.SANS_SERIF, Typeface.BOLD));
         headerPaint.setTextSize(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 24, getResources().getDisplayMetrics()));
+        headerPaint.setColor(Color.WHITE);
 
         bgPaint = new Paint();
         bgPaint.setColor(Color.TRANSPARENT);
@@ -71,24 +82,37 @@ public class RecognitionScoreView extends View implements ResultsView {
         final int x = 10;
         int y = (int) (fgPaint.getTextSize() * 1.5f);
 
-        canvas.drawPaint(bgPaint);
 
         Set customPositionedAttributes = new HashSet();
         customPositionedAttributes.add("description");
         customPositionedAttributes.add("id");
         if (image != null) {
+            ImageView view = (ImageView) getRootView().findViewById(R.id.imageView);
+            view.setVisibility(VISIBLE);
+
             String fileName = image.getOws().getFileName();
             String key = fileName.substring(0, fileName.indexOf(".jpg"));
             Map<String, String> data = Constants.objectDataFromRetailMe.get(key);
 
-            canvas.drawText(data.get("description") + " (" + data.get("id") + ")", 10, 70, headerPaint);
+            canvas.drawText(data.get("description"), 80, 1440, headerPaint);
 
-            for (String left : data.keySet()) {
-                if (!customPositionedAttributes.contains(left)) {
-                    canvas.drawText(left.toUpperCase() + ": " + data.get(left), x, y + 100, fgPaint);
-                    y += fgPaint.getTextSize() * 1.5f;
-                }
-            }
+            canvas.drawText("Sell Through", 80, 1540, fgPaintHeader);
+            canvas.drawText(data.get("Sell Through %"), 80, 1600, fgPaint);
+
+
+            canvas.drawText("Margin", 80, 1670, fgPaintHeader);
+            canvas.drawText(data.get("Margin %"), 80, 1730, fgPaint);
+
+
+            canvas.drawText("Buy Quantity", 580, 1540, fgPaintHeader);
+            canvas.drawText(data.get("Buy Qty"), 580, 1600, fgPaint);
+
+
+            canvas.drawText("IMU", 580, 1670, fgPaintHeader);
+            canvas.drawText(data.get("IMU%"), 580, 1730, fgPaint);
+        } else {
+            ImageView view = (ImageView) getRootView().findViewById(R.id.imageView);
+            view.setVisibility(INVISIBLE);
         }
 
         y = 900;
